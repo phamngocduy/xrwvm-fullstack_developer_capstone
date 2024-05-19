6f1b549970fa3efb81c68f97af33a941a453902d
phamngocduy <phmngcduy@gmail.com>
2024-05-19 05:10:31 -0400

    Adds CarMake CarModel

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,8 +13,17 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
+from .models import CarMake, CarModel
 
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if count == 0:
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = [{'CarModel': car_model.name, 'CarMake': car_model.car_make.name} for car_model in car_models]
+    return JsonResponse({'CarModels': cars})
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
--- server/runserver.sh
+++ server/runserver.sh
@@ -7,7 +7,7 @@ source djangoenv/bin/activate
 pip3 install -U -r requirements.txt
 
 python3 manage.py makemigrations
-python3 manage.py migrate
+python3 manage.py migrate --run-syncdb
 
 python3 manage.py runserver
 alias py=python3
